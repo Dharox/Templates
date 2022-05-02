dc5585847e885e194bc05bac9cc17c634a44239e
Daniel Breslin <daniel.breslin@sinclair.edu>
2022-05-02 13:22:13 -0400

    - main filled in to mirror sample from instructions - started logic for template - think it's correct but will have to finish in function and test again

--- Templates/Source.cpp
+++ Templates/Source.cpp
@@ -10,11 +10,26 @@ Templates
 
 using namespace std;
 
+template<typename T>
+T Half(T num)
+{
+	T answer;
+
+	answer = num / 2;
+
+	return answer;
+}
+
+
 int main()
 {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
 
-
-
+	cout << Half(a) << endl;
+	cout << Half(b) << endl;
+	cout << Half(c) << endl;
 
 
 	return 0;
